2dfb22260001886316c3fe1a658b260fa9c41039
Agata Kozińska <kozinska.agata@gmail.com>
2023-05-09 15:01:02 +0200

    UXFCP-3588 (feat): enable wgCategories for mobile

--- includes/MobileFrontend.hooks.php
+++ includes/MobileFrontend.hooks.php
@@ -1107,7 +1107,6 @@ class MobileFrontendHooks {
 		// If the device is a mobile, Remove the category entry.
 		$context = MobileContext::singleton();
 		if ( $context->shouldDisplayMobileView() ) {
-			unset( $vars['wgCategories'] );
 			$vars['wgMFMode'] = $context->isBetaGroupMember() ? 'beta' : 'stable';
 			$vars['wgMFLazyLoadImages'] =
 				$featureManager->isFeatureAvailableInContext( 'MFLazyLoadImages', $context );
